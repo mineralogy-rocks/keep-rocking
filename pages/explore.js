e7a3ec4f8bea9556aa310c67f4f04b8196491343
liubomyr.gavryliv <liubomyr.gavryliv@gmail.com>
2022-10-02 22:42:16 +0200

    init project

---
+++ pages/explore.js
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+import Link from 'next/link';
+
+export default function Explore() {
+  return (
+    <div className='bg-gray-500 border-b-2 border-b-neutral-600'>
+      <Head>
+        <title>Explore</title>
+      </Head>
+      <div>Explore</div>
+      <Link href='/'>home</Link>
+    </div>
+  );
+}
