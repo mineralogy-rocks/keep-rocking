d93e9b5e8230d60791eb413f0608971149694c88
Liubomyr <liubomyr.gavryliv@gmail.com>
2023-02-15 21:34:50 +0100

    added error, loading handling to index

--- components/MineralCard/Classification.tsx
+++ components/MineralCard/Classification.tsx
@@ -1,16 +1,14 @@
 
-import { useState, Fragment, useRef } from 'react';
+import { useState } from 'react';
 
 import clsx from 'clsx';
-import { usePopper } from 'react-popper';
-import { Popover, Transition } from '@headlessui/react'
 
 import { getIMAStatusColor } from '@/helpers/status.helpers';
 import { camelize } from '@utils';
 import { useMindatApi } from '@/hooks/use-mindat-api';
 
 import Chip from '@/components/Chip';
-import QuestionIcon from './QuestionIcon';
+import Tooltip from './Tooltip';
 import  { NoData }  from './MineralCard';
 
 
@@ -21,35 +19,13 @@ export default function ClassificationSnippet({ data }) {
     setIndex(newIndex);
   };
 
-  const popperRef = useRef(null);
-  const [referenceElement, setReferenceElement] = useState(null);
-  const [popperElement, setPopperElement] = useState(null);
-  const { styles, attributes } = usePopper(referenceElement, popperElement, {
-    placement: 'bottom',
-    modifiers: [
-      {
-        name: 'offset',
-        options: {
-          offset: [0, 8],
-        },
-      },
-      {
-        name: 'flip',
-        options: {
-          fallbackPlacements: ['top', 'bottom'],
-        },
-      },
-    ],
-  });
-
-
   const { data: _data, error, isLoading } = useMindatApi(
     index ? `/nickel-strunz-10/families/?index=${index}` : null
   );
   let _fields = ['strunz_index', 'dana_index', 'ima_status',];
 
   if (data && _fields.some(key => Object.keys(data).includes(key)) && _fields.some(key => data[key])) {
-    const { strunz_index, dana_index, ima_status } = data;
+    const { ns_index, strunz_index, dana_index, ima_status } = data;
     return (
       <div className="flex flex-col">
         <div className="flex w-full">
@@ -67,63 +43,38 @@ export default function ClassificationSnippet({ data }) {
             </div>
           )}
         </div>
-        {strunz_index && (
-          <div className="flex items-center flex-wrap text-xs mt-1">
+        {/* {ns_index && (
+          <div className="flex flex-wrap items-center text-xs mt-1">
             <h4 className="font-medium mr-1">Nickel-Strunz (10th)</h4>
             <div className="flex items-center rounded py-0.5">
-              <span className="">{strunz_index}</span>
-              <Popover className="relative">
-                {({ open, close }) => {
-                  return (
-                    <>
-                      <Popover.Button ref={setReferenceElement} className="flex outline-none">
-                        <QuestionIcon className={open ? "stroke-cyan-900 transition-colors duration-1000" : "stroke-gray-400"} onClick={(e) => {e && handleIndexUpdate(strunz_index)}} />
-                      </Popover.Button>
-                      <div ref={popperRef}
-                           style={styles.popper}
-                           {...attributes.popper}>
-                        <Transition as={Fragment}
-                                    show={open && !!_data?.results}
-                                    enter="transition duration-500 ease-out"
-                                    enterFrom="transform translate-y-5 opacity-0"
-                                    enterTo="transform translate-y-0 opacity-100"
-                                    leave="transition duration-100 ease-out"
-                                    leaveFrom="transform translate-y-0 opacity-100"
-                                    leaveTo="transform translate-y-5 opacity-0"
-                                    beforeEnter={() => setPopperElement(popperRef.current)}
-                                    afterLeave={() => setPopperElement(null)}>
-                          <Popover.Panel as="div"
-                                         static
-                                         className="w-screen max-w-xs md:max-w-sm z-10 bg-zinc-50 shadow-lg shadow-gray-200 border-[0.5px] border-gray-200 rounded-md p-1">
-                            <>
-                              {_data?.results &&
-                                (<div className="relative flex flex-col space-y-1 p-1">
-                                  <button type="button" onClick={(e) => { e.preventDefault(); close() }} className="absolute top-1 right-1">
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 stroke-gray-100">
-                                      <path d="M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L11.06 10l3.72-3.72a.75.75 0 00-1.06-1.06L10 8.94 6.28 5.22z" />
-                                    </svg>
-                                  </button>
+              <span className="">{ns_index}</span>
+              </div>
+          </div>
+        )} */}
+        {strunz_index && (
+          <div className="flex flex-wrap items-center text-xs mt-1">
+            <h4 className="font-medium mr-1">Nickel-Strunz (10th)</h4>
 
-                                  <p className="font-semibold pb-2 mr-5">Nickel-Strunz (2023) Classification of mindat.org</p>
-                                  <div><span className="font-semibold">{_data?.results[0].strunz1}:</span><span className="ml-1">{_data?.results[0].title1}</span></div>
-                                  <div><span className="font-semibold">{_data?.results[0].strunz2}:</span><span className="ml-1" dangerouslySetInnerHTML={{ __html: _data?.results[0].title2 }}></span></div>
-                                  <div><span className="font-semibold">{_data?.results[0].strunz3}:</span><span className="ml-1" dangerouslySetInnerHTML={{ __html: _data?.results[0].title3 }}></span></div>
-                                </div>)
-                              }
-                            </>
-                          </Popover.Panel>
-                        </Transition>
-                      </div>
-                    </>
-                  )}
+            <div className="flex items-center rounded py-0.5">
+              <span className="">{strunz_index}</span>
+              <Tooltip key={strunz_index} onClick={(e) => {e && handleIndexUpdate(strunz_index)}} isShown={!!_data} isLoading={isLoading} error={error}>
+                {_data?.results &&
+                  (<div className="relative flex flex-col space-y-1 p-1">
+                    <div className="border-b">
+                      <p className="font-semibold pb-2 mr-5">Nickel-Strunz (2023) Classification of mindat.org</p>
+                    </div>
+                    <div><span className="font-semibold">{_data?.results[0].strunz1}:</span><span className="ml-1">{_data?.results[0].title1}</span></div>
+                    <div><span className="font-semibold">{_data?.results[0].strunz2}:</span><span className="ml-1" dangerouslySetInnerHTML={{ __html: _data?.results[0].title2 }}></span></div>
+                    <div><span className="font-semibold">{_data?.results[0].strunz3}:</span><span className="ml-1" dangerouslySetInnerHTML={{ __html: _data?.results[0].title3 }}></span></div>
+                  </div>)
                 }
-              </Popover>
+              </Tooltip>
             </div>
           </div>
         )}
         {dana_index && (
           <div className="flex flex-wrap items-center text-xs mt-1">
-            <h4 className="font-medium mr-1">Dana</h4>
+            <h4 className="font-medium mr-1">Dana (8th)</h4>
             <div className="flex items-center rounded px-1 py-0.5">
               <span className="">{dana_index}</span>
               {/* <QuestionIcon mouseEventHandler={(e) => e && console.log(dana_index)} /> */}
