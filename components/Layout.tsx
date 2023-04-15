352d1aca66d70c9cb559dcf9a7727aa20660d499
Liubomyr <liubomyr.gavryliv@gmail.com>
2023-04-15 10:08:06 +0200

    added font to globals

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -2,18 +2,18 @@ import NavBar from './NavBar';
 import Footer from './Footer';
 
 import clsx from 'clsx';
-import { Inter } from '@next/font/google';
+// import { Inter } from '@next/font/google';
 
-const inter = Inter({
-  subsets: ['latin'],
-  variable: '--font-inter',
-});
+// const inter = Inter({
+//   subsets: ['latin'],
+//   variable: '--font-inter',
+// });
 
 export default function Layout({ children }) {
   return (
     <>
       <NavBar />
-        <main className={clsx("max-w-full min-h-[70vh] mx-auto relative pt-10 text-font", "font-sans", inter.variable)}>{children}</main>
+        <main className={clsx("max-w-full min-h-[70vh] mx-auto relative pt-10 text-font")}>{children}</main>
       <Footer />
     </>
   );
