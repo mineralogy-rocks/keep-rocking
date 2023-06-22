496de6bcd6926170d7dfdedb4d8c53f90d1a3a8a
Liubomyr <liubomyr.gavryliv@gmail.com>
2023-06-22 08:34:52 +0200

    app WIP

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,31 +2,37 @@ import Head from 'next/head';
 import type { AppProps } from 'next/app';
 
 import Layout from '../components/Layout';
+import NavBar from '../components/NavBar';
+import Footer from '../components/Footer';
 import '../styles/globals.scss';
 
 import { Inter } from '@next/font/google';
 
+import clsx from 'clsx';
+
+
 const inter = Inter({
   subsets: ['latin'],
   variable: '--font-inter',
+  weights: [100, 200, 300, 400, 500, 600, 700, 800, 900],
 });
 
-export default function MyApp({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <style jsx global>{`
-        html {
-          font-family: ${inter.style.fontFamily};
-
-        }
-      `}</style>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
 
-      <Layout>
+
+      <main className={clsx("max-w-full min-h-[70vh] mx-auto relative pt-10 text-font font-sans", inter.variable)}>
+        <NavBar />
         <Component {...pageProps} />
-      </Layout>
+        <Footer />
+      </main>
+      {/* <Layout>
+        <Component {...pageProps} />
+      </Layout> */}
     </>
   )
 };
