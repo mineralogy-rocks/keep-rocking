2090980c5f73daa6492ac30943bb5b685cd9baba
Liubomyr Gavryliv <liubomyr.gavryliv@gmail.com>
2024-02-29 16:40:32 +0100

    force dynamic

--- app/explore/page.tsx
+++ app/explore/page.tsx
@@ -6,6 +6,7 @@ import { exploreApiRequest, exploreApiResponse } from "@/lib/types";
 
 import Search from './search';
 
+export const dynamic = 'force-dynamic';
 
 export default async function Explore({ searchParams }: { searchParams: exploreApiRequest }) {
   const cleanQuery = filter(searchParams, (key, value) => value !== '' && value !== null);
