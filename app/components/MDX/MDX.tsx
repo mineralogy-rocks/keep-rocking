cad676f73b7fee3be543b7422c75ad4fb45d6823
Liubomyr <liubomyr.gavryliv@gmail.com>
2024-05-08 19:43:00 +0200

    move slugify to utils

--- app/components/MDX/MDX.tsx
+++ app/components/MDX/MDX.tsx
@@ -5,23 +5,13 @@ import Image from 'next/image';
 import { MDXRemote } from 'next-mdx-remote/rsc';
 import { highlight } from 'sugar-high';
 
+import { slugify } from "@utils";
 import Heading from '@/components/Heading';
 
 
 function Code({ children, ...props }) {
   const codeHTML = highlight(children);
   return <code dangerouslySetInnerHTML={{ __html: codeHTML }} {...props} />;
-}
-
-function slugify(str: string) {
-  return str
-    .toString()
-    .toLowerCase()
-    .trim() // Remove whitespace from both ends of a string
-    .replace(/\s+/g, '-') // Replace spaces with -
-    .replace(/&/g, '-and-') // Replace & with 'and'
-    .replace(/[^\w\-]+/g, '') // Remove all non-word characters except for -
-    .replace(/\-\-+/g, '-'); // Replace multiple - with single -
 }
 
 function createHeading(level) {
--- utils/index.ts
+++ utils/index.ts
@@ -35,6 +35,17 @@ export const concatStrings = (values: string[], sep: string=" "): string => {
   return values.filter((v) => v).join(sep);
 };
 
+export const slugify = (str: string): string => {
+  return str
+    .toString()
+    .toLowerCase()
+    .trim() // Remove whitespace from both ends of a string
+    .replace(/\s+/g, '-') // Replace spaces with -
+    .replace(/&/g, '-and-') // Replace & with 'and'
+    .replace(/[^\w\-]+/g, '') // Remove all non-word characters except for -
+    .replace(/\-\-+/g, '-'); // Replace multiple - with single -
+};
+
 export const compareColors = [
 	{
 		base: '#2563eb',
