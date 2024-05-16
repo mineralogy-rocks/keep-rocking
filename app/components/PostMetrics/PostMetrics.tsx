2a47957b8240267c003431123a1baba5c6d20b4a
Liubomyr <liubomyr.gavryliv@gmail.com>
2024-05-16 21:19:52 +0200

    check prefetch header

--- app/components/PostMetrics/PostMetrics.tsx
+++ app/components/PostMetrics/PostMetrics.tsx
@@ -1,5 +1,6 @@
 'use server';
 import {cache} from "react";
+import { headers } from 'next/headers';
 import { unstable_noStore as noStore } from 'next/cache';
 
 import {getViews, incrementViews as increment} from "@/actions";
@@ -17,9 +18,13 @@ const defaultProps = {
 
 const PostMetrics = async function PostMetrics(props: Props) {
   noStore();
+  const headersList = headers();
+  const isPrefetch = headersList.get('Next-Router-Prefetch') === '1';
   const { slug } = { ...defaultProps, ...props};
 
-  await incrementViews(slug);
+  if (!isPrefetch) {
+    await incrementViews(slug);
+  }
   const views = await getViews(slug);
 
   return (<p className="text-xs md:text-sm text-font-secondary slashed-zero tabular-nums">{views?.count.toLocaleString()} views</p>);
