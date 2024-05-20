3c8a4540e7bb9b34f4c0ed54ca8a459e9c6b15b0
Liubomyr Gavryliv <liubomyr.gavryliv@gmail.com>
2024-05-20 16:59:17 +0200

    added robots  WIP

---
+++ app/robots.ts
@@ -0,0 +1,11 @@
+export default function robots() {
+  return {
+    rules: [
+      {
+        userAgent: '*',
+      },
+    ],
+    // sitemap: 'https://mineralogy.rocks/sitemap.xml',
+    host: 'https://mineralogy.rocks',
+  };
+}
